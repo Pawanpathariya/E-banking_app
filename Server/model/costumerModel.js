514592e98b442519019b1227f6baf423fa5183fb
Pawanpathariya <pawanpathariys@gmail.com>
2025-03-25 10:03:56 +0530

    p

--- Server/model/costumerModel.js
+++ Server/model/costumerModel.js
@@ -1,14 +1,14 @@
 const mongoose=require("mongoose")
 const costumerSchema=new mongoose.Schema({
     name:{type:String,required:true},
-    fathername:{type:String,required:true},
-    address:{type:String,required:true},
+    fathername:{type:String},
+    address:{type:String},
     email:{type:String,required:true,unique:true},
-    city:{type:String,required:true},
+    city:{type:String},
     mobile:{type:String,required:true},
-    pincode:{type:String,required:true},
+    pincode:{type:String},
     accountType:{type:String,required:true},
-    password:{type:String,required:true},
+    password:{type:String},
     createdAt: { type: Date, default: Date.now},
     accountID:{type:mongoose.Schema.Types.ObjectId,ref:"account"}
 })
